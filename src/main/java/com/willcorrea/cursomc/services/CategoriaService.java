5f5d4494505c228861669004d7923ac99ecb3259
Willian Correa <williantoshio14@gmail.com>
2022-08-15 22:47:58 -0300

    fixing endpoint /categorias/{id}

--- src/main/java/com/willcorrea/cursomc/services/CategoriaService.java
+++ src/main/java/com/willcorrea/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.willcorrea.cursomc.domain.Categoria;
 import com.willcorrea.cursomc.repositories.CategoriaRepository;
+import com.willcorrea.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,11 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+			
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + 
+				"\nTipo: " + Categoria.class.getName()
+				));
 	}
 	
 }
